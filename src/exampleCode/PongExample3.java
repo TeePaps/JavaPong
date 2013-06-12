fa41ea016d21d04f5ac6464f4eed8f2d5b94212a
Ted Papaioannou <TedDev@gastrograph.com>
2013-06-12 18:56:20 -0400

    Added Sally's changes

--- src/exampleCode/PongExample3.java
+++ src/exampleCode/PongExample3.java
@@ -1,62 +1,54 @@
 package exampleCode;
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 
 public class PongExample3 extends JFrame{
 	private static final long serialVersionUID = 1L;
-	private JPanel jContentPane = null;
-	private TwoPlayerPong panel = null;
-	private TwoPlayerPong getPanel(){
-		if(panel == null){
-			panel = new TwoPlayerPong();
-		}
-		return panel;
+	protected JPanel panel = new JPanel();
+	protected NewOnePlayerPong game = new NewOnePlayerPong();
+	int xsize = 800;
+	int ysize = 600;
+	
+	public PongExample3(){
+		super("RTS_Pong");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setMinimumSize(new Dimension(xsize, ysize));
+		setMaximumSize(new Dimension(xsize, ysize));
+		setBounds(new Rectangle(50, 50, xsize, ysize));
+		setResizable(false);
+		panel.setLayout(new BorderLayout());
+		panel.add(game, BorderLayout.CENTER);
+		setContentPane(panel);
 		
+		this.addKeyListener(new KeyAdapter(){
+			public void keyPressed(KeyEvent e){
+				formKeyPressed(e);
+			}
+			public void keyReleased(KeyEvent e){
+				formKeyReleased(e);
+			}
+		});
+		
+		pack();
+	} //end constructor
+	
+	protected void formKeyPressed(KeyEvent e){
+		game.KeyPressed(e);
 	}
-
-public PongExample3(){
-	super();
-	initialize();
-	this.addKeyListener(new KeyAdapter(){
-		public void keyPressed(KeyEvent e){
-			formKeyPressed(e);
-		}
-		public void keyReleased(KeyEvent e){
-			formKeyReleased(e);
-		}
-	});
+	
+	protected void formKeyReleased(KeyEvent e){
+		game.keyReleased(e);
 	}
-	private void formKeyPressed(KeyEvent e){
-		panel.KeyPressed(e);
-	}
-	private void formKeyReleased(KeyEvent e){
-		panel.keyReleased(e);
-	}
-	private void initialize(){
-		this.setResizable(false);
-		this.setBounds(new Rectangle(400, 400, 400, 400));
-		this.setMaximumSize(new Dimension(400, 400));
-		this.setMinimumSize(new Dimension(400, 400));
-		this.setContentPane(getJContentPane());
-		this.setTitle("RTS_Pong");
-	}
-	private JPanel getJContentPane(){
-		if(jContentPane == null){
-			jContentPane = new JPanel();
-			jContentPane.setLayout(new BorderLayout());
-			jContentPane.add(getPanel(), BorderLayout.CENTER);
-		}
-		return jContentPane;
-	}
+	
 	public static void main(String[] args){
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run(){
 				PongExample3 thisClass = new PongExample3();
-				thisClass.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				thisClass.setVisible(true);
 			}
 		});
-	}
-}
+	} //end main
+} //end class
